f1d2f6e2f469b6a2ca15ad8747c590f5db4dfe02
Jake Tran <jake.tranehs@gmail.com>
2019-10-09 12:46:22 -0700

    hello

--- src/markov.py
+++ src/markov.py
@@ -1,83 +1,62 @@
 import sys, random, string
 from dictogram import Dictogram
-sys.path.insert(0, '../Tweet_Generator')
 from sample import weighted_random_choice
+from helper import read_textfile, convert_to_list
 
 class Markov(dict):
-
     def __init__(self, word_list=None, order=1):
-        ''' markov class initialization'''
         super(Markov, self).__init__()
-
         self.order = order
 
-    def create_markov_model(self, word_list):
-
+    def initiate_markov(self, word_list):
         order = self.order
-
-        #iterate through the word list
+        # iterate through the word_list to check key
         for i in range(len(word_list) - self.order):
             key = tuple(word_list[i: i + order])
             value = word_list[i + order]
-            self.check_key(key, value)
-
-    def check_key(self, key, value):
-        if key in self:
-            self[key].add_count(value)
-        else:
-            self[key] = Dictogram([value])
+            if key in self:
+                self[key].add_count(value)
+            else:
+                self[key] = Dictogram([value])
 
     def generate_sentence(self, count=10):
 
-        # Find random key
+        # Find random word
         rand_string = ""
-        rand_key = random.choice(list(self))
-        rand_followup = weighted_random_choice(self[rand_key])
-        rand_string = rand_string + ' '.join(rand_key) + ' ' + rand_followup
+        random_word = random.choice(list(self))
+        rand_followup = weighted_random_choice(self[random_word])
+        rand_string = rand_string + ' '.join(random_word) + ' ' + rand_followup
 
-        # add follow-up word to tuple, chop off head
+        # add follow-up word to tuple
         for i in range(count - self.order - 1):
 
-            tmp_list = list(rand_key)
-            tmp_list.append(rand_followup)
-            tmp_list = tmp_list[1:]
-            rand_key = tuple(tmp_list)
+            temp = list(random_word)
+            temp.append(rand_followup)
+            temp = temp[1:]
+            random_word = tuple(temp)
 
             # Handle KeyError
             try:
-                rand_followup = weighted_random_choice(self[rand_key])
+                rand_followup = weighted_random_choice(self[random_word])
             except KeyError:
-                rand_key = random.choice(list(self))
-                rand_followup = weighted_random_choice(self[rand_key])
+                random_word = random.choice(list(self))
+                rand_followup = weighted_random_choice(self[random_word])
 
                 # Edge Case
             while rand_followup == None:
-                rand_followup = weighted_random_choice(self[rand_key])
+                rand_followup = weighted_random_choice(self[random_word])
                 print(rand_followup)
 
             rand_string = rand_string + " " + rand_followup
 
         return rand_string
 
-def read_in_txtfile(text):
-    '''reads in from text file and saves to an array'''
-    with open(text) as f:
-        word_data = f.read()
-    return word_data
-
-def convert_to_list(text_string):
-    '''Takes in string of text, converts to and returns a list'''
-    word_list = text_string.split()
-    return word_list
-
 def main():
-    txtfile = read_in_txtfile("sample_text.txt")
-    txt_list = convert_to_list(txtfile)
-    markov_chain = Markov(txt_list, 3)
-    markov_chain.create_markov_model(txt_list)
-    # string = markov_chain.generate_sentence(30)
-
-    return markov_chain
+    text = read_textfile("sample_text.txt")
+    txt_list = convert_to_list(text)
+    markov_chain = Markov(txt_list, 2)
+    markov_chain.initiate_markov(txt_list)
+    print(markov_chain.generate_sentence(15))
 
 if __name__ == '__main__':
     main()
--- src/sample.py
+++ src/sample.py
@@ -1,20 +1,17 @@
 import sys, random
-from dictionary_words import read_in_txtfile, convert_to_list
-from word_frequency_dict import histogram
+from helper import read_textfile, convert_to_list
+from word_frequency import histogram
 
 
 def get_random_word(histogram):
-    '''Takes in histogram, returns random word '''
+    '''Takes in histogram, returns random word'''
     rand_word = random.choice(list(histogram.keys()))
     return rand_word
 
-def get_sample(storage_list):
+def get_sample(given_list):
     '''builds a sample dictionary using a list'''
-
-    '''dictionary'''
     sample_list = {}
-
-    for word in storage_list:
+    for word in given_list:
         if sample_list.get(word) is None:
             sample_list[word] = 1
         else:
@@ -22,51 +19,25 @@ def get_sample(storage_list):
     return sample_list
 
 def weighted_random_choice(histogram):
-    '''This function will return a random word based on a
-    weighted probability -use random.randint'''
+    '''This function return random word based on weighted list'''
 
     # converts list of keys and values
     words, weights = zip(*histogram.items())
-
     weight_list = []
-
     curr_weight = 0
     for weight in weights:
         curr_weight += weight
         weight_list.append(curr_weight)
-
     rand_num = random.randint(1, curr_weight)
-
     for word, weight in zip(words, weight_list):
         if rand_num <= weight:
             return word
 
-def test_uniform_randomness(histogram):
-    uniform_list = []
-    for i in range(10000):
-        uniform_list.append(get_random_word(histogram))
-    test = get_sample(uniform_list)
-    print(test)
-
-def test_weighted_randomness(histogram):
-    weighted_list = []
-    for i in range(10000):
-        rand_word = weighted_random_choice(histogram)
-        weighted_list.append(rand_word)
-    weighted_sample = get_sample(weighted_list)
-    print(weighted_sample)
-
 def main():
-    '''This program reads in a histogram and returns a random word'''
-    #read in text file, store as list
-    txt = read_in_txtfile("short_story.txt")
+    '''Return random word from histogram'''
+    txt = read_textfile("sample_text.txt")
     txt_list = convert_to_list(txt)
-
-    #create histogram based on list
-    my_histogram = histogram(txt_list)
-
-    test_uniform_randomness(my_histogram)
-    test_weighted_randomness(my_histogram)
+    histogram1 = histogram(txt_list)
 
 if __name__ == '__main__':
     main()
